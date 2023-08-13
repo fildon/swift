f9f8fe40e00ccd2a1574c5df27454a9aaaa19b4b
Rupert Foggo McKay <maxfmckay@gmail.com>
2023-08-13 13:54:09 +0200

    🙂 Swift's face in the animation

--- index.js
+++ index.js
@@ -4,6 +4,19 @@
 const WIDTH = 800;
 const HEIGHT = 360;
 
+/**
+ * The actual image is 626x742
+ *
+ * This is the same ratio as: 312x371
+ *
+ * Which is roughly the same as:
+ *
+ * ~ 156x185
+ * ~ 50x60 = 600x720
+ */
+const swifthead = new Image();
+swifthead.src = "swifthead.png";
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -18,16 +31,7 @@ const animationFrame = (timestamp) => {
   ctx.rect(0, 0, WIDTH, HEIGHT);
   ctx.fill();
 
-  ctx.beginPath();
-  ctx.fillStyle = "red";
-  ctx.arc(
-    (timeParam % (WIDTH + 40)) - 20,
-    (timeParam % (HEIGHT + 40)) - 20,
-    20,
-    0,
-    2 * Math.PI
-  );
-  ctx.fill();
+  ctx.drawImage(swifthead, 100, timeParam % HEIGHT, 50, 60);
 
   requestAnimationFrame(animationFrame);
 };

1b2aee870e354b744ff457237c700b26fe060a76
Rupert Foggo McKay <maxfmckay@gmail.com>
2023-08-13 14:37:07 +0200

    🦘 Jump control

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * The canvas is locked at 800 wide and 360 high
  */
-const WIDTH = 800;
-const HEIGHT = 360;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 360;
 
 /**
  * The actual image is 626x742
@@ -20,20 +20,67 @@ swifthead.src = "swifthead.png";
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const animationFrame = (timestamp) => {
-  const SPEED = 0.1;
-  const timeParam = SPEED * timestamp;
+/**
+ * We model jumping state by remembering the last time the user started a jump.
+ */
+let jump_start_timestamp = -Infinity;
+const JUMP_DURATION = 1000; // 1000ms = 1seconds
 
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+/**
+ * We are jumping if the most recent jump was less than a full jump duration ago.
+ */
+const isJumping = () =>
+  performance.now() - jump_start_timestamp < JUMP_DURATION;
+
+/**
+ * Get the current player height.
+ *
+ * If we are not jumping, they are at a fixed height.
+ *
+ * If they are jumping we use a sine curve to model their position rising and falling over time.
+ */
+const getPlayerHeight = () => {
+  if (!isJumping()) return CANVAS_HEIGHT / 3;
+  // Milliseconds since the jump started
+  const timeSinceJump = performance.now() - jump_start_timestamp;
+  // 0 to 1 proportion of jump completed
+  const jumpDurationComplete = timeSinceJump / JUMP_DURATION;
+  // The positive first half of a sine curve.
+  // Amplitude runs from HEIGHT/3 to 2*(HEIGHT/3).
+  // Wavelength is JUMP_DURATION*2 (since we only use half the wave to represent a jump)
+  return (
+    CANVAS_HEIGHT / 3 +
+    (CANVAS_HEIGHT / 3) * Math.sin(Math.PI * jumpDurationComplete)
+  );
+};
+
+const animationFrame = () => {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
   ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.rect(0, 0, WIDTH, HEIGHT);
+  ctx.fillStyle = "darkgreen";
+  ctx.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.fill();
 
-  ctx.drawImage(swifthead, 100, timeParam % HEIGHT, 50, 60);
+  /**
+   * We have to invert height here.
+   *
+   * Our game data model has "height" as going up.
+   * But when drawing to canvas,
+   * higher y values correspond to the downwards direction.
+   */
+  ctx.drawImage(swifthead, 100, CANVAS_HEIGHT - getPlayerHeight(), 50, 60);
 
   requestAnimationFrame(animationFrame);
 };
+
+const handleJumpInput = () => {
+  if (isJumping()) return;
+  jump_start_timestamp = performance.now();
+};
+document.addEventListener("click", handleJumpInput);
+document.addEventListener("keyup", (e) =>
+  e.code === "Space" ? handleJumpInput() : null
+);
 
 requestAnimationFrame(animationFrame);
